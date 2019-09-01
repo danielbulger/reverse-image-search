Fixed issues with L2 Normalisation dimensions and changed all flatten operators to use torch.flatten instead of view

model/ConvNet.py
@@ -1,4 +1,4 @@
-from torch import nn
+from torch import nn, flatten
 import torchvision.models as models
 import torch.nn.functional as F
 
@@ -31,7 +31,7 @@ class ConvNet(nn.Module):
         x = self.model(x)
 
         # Flatten the output of the CNN
-        x = x.view(x.size()[0], -1)
+        x = flatten(x)
 
         x = self.relu(self.fc1(x))
         x = self.dropout(x)
@@ -40,4 +40,4 @@ class ConvNet(nn.Module):
         x = self.dropout(x)
 
         # L2 normalize the output
-        return F.normalize(x, p=2, dim=1)
+        return F.normalize(x, p=2, dim=0)

model/DeepRank.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from model.ConvNet import ConvNet
 from model.Rank import Rank
+import torch.nn.functional as F
 
 
 class DeepRank(nn.Module):
@@ -9,17 +10,12 @@ class DeepRank(nn.Module):
     def __init__(self):
         super(DeepRank, self).__init__()
         self.convnet = ConvNet()
-        self.rank1 = Rank(conv_strides=16, pool_size=3, pool_strides=4)
-        self.rank2 = Rank(conv_strides=32, pool_size=7, pool_strides=2)
+        self.rank1 = Rank(conv_strides=(16, 16), pool_size=(3, 3), pool_strides=(4, 4))
+        self.rank2 = Rank(conv_strides=(32, 32), pool_size=(7, 7), pool_strides=(2, 2))
+        self.embedding = nn.Linear(in_features=5056, out_features=4096)
 
     def forward(self, x):
-        conv_output = self.convnet(x)
-        rank1_output = self.rank1(x)
-        rank2_output = self.rank2(x)
-
-        rank_cat = torch.cat(rank1_output, rank2_output)
-        combine = torch.cat(rank_cat, conv_output)
-
-        print(combine.size())
-
-        return combine
+        rank_cat = torch.cat((self.rank1(x), self.rank2(x)))
+        x = torch.cat((rank_cat, self.convnet(x)))
+        x = self.embedding(x)
+        return F.normalize(x, p=2, dim=0)

model/Rank.py
@@ -1,4 +1,4 @@
-from torch import nn
+from torch import nn, flatten
 import torch.nn.functional as F
 
 
@@ -6,11 +6,11 @@ class Rank(nn.Module):
 
     def __init__(self, conv_strides, pool_size, pool_strides):
         super(Rank, self).__init__()
-        self.conv = nn.Conv2d(3, 96, kernel_size=8, stride=conv_strides)
+        self.conv = nn.Conv2d(3, 96, kernel_size=(8, 8), stride=conv_strides)
         self.pool = nn.MaxPool2d(pool_size, stride=pool_strides)
 
     def forward(self, x):
         x = self.conv(x)
         x = self.pool(x)
-        x = x.view(x.size()[0], -1)
-        return F.normalize(x, p=2, dim=1)
+        x = flatten(x)
+        return F.normalize(x, p=2, dim=0)