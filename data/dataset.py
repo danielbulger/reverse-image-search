57d6aff66883d5467869b13cd7c75577f79a7e58
Daniel Bulger <68453486+danielbulger@users.noreply.github.com>
2019-09-21 09:50:49 +1000

    Started implementing training & run code

--- data/dataset.py
+++ data/dataset.py
@@ -1,11 +1,83 @@
+import os
+from torch.utils import data
 from torchvision import transforms
+from PIL import Image
 
 
-def get_train_transforms():
+def get_images(root_dir):
+    """
+    Gets all the images from a directory recursively.
+    :param root_dir: The starting directory to search for images.
+    :return: A list of image files.
+    """
+    images = []
+    for dir, subdirs, files in os.walk(root_dir):
+        for file in files:
+            if file.endswith(('jpg', 'jpeg', 'png')):
+                images.append(os.path.join(dir, file))
+
+    return images
+
+
+class FolderDataSet(data.Dataset):
+
+    def __init__(self, root_dir, crop_size):
+        """
+        Creates a new dataset of images from the provided root directory.
+        :param root_dir: The folder to search for images.
+        :param crop_size: The size to crop the images to.
+        """
+        super(FolderDataSet, self).__init__()
+        self.files = get_images(root_dir)
+        self.input_transform = get_train_transforms(crop_size)
+        self.target_transform = get_train_transforms(crop_size)
+        self.flip_transform = get_flip_transforms(crop_size)
+
+    def __getitem__(self, index):
+        train = Image.open(self.files[index]).convert('RGB')
+        flipped = train.copy()
+
+        self.input_transform(train)
+        self.flip_transform(flipped)
+
+        return train, flipped
+
+    def __len__(self):
+        return len(self.files)
+
+
+def get_flip_transforms(crop_size):
     return transforms.Compose([
-        transforms.RandomPerspective(),
-        transforms.RandomVerticalFlip(),
-        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(crop_size),
+        transforms.RandomHorizontalFlip(1),
         transforms.ToTensor()
     ])
 
+
+def get_train_transforms(crop_size):
+    return transforms.Compose([
+        transforms.CenterCrop(crop_size),
+        transforms.Resize(crop_size),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor()
+    ])
+
+
+def get_target_transforms(crop_size):
+    return transforms.Compose([
+        transforms.CenterCrop(crop_size),
+        transforms.ToTensor()
+    ])
+
+
+def get_train_dataset(root, crop_size):
+    """
+    Get the training dataset.
+    :param root: The folder to search for images in.
+    :param crop_size
+    :return:
+    """
+    return FolderDataSet(
+        root_dir=root,
+        crop_size=crop_size
+    )
--- main.py
+++ main.py
@@ -1,21 +1,55 @@
+from argparse import ArgumentParser
+
 import torch
-from model.DeepRank import DeepRank
+from data.dataset import get_images
 from PIL import Image
 
 from torch.autograd import Variable
 from torchvision.transforms import ToTensor
+from annoy import AnnoyIndex
 
 
+def parse_args():
+    parser = ArgumentParser()
+
+    parser.add_argument('--cuda', default=False, type=lambda x: (str(x).lower() == 'true'),
+                        help="Whether to train using CUDA")
+    parser.add_argument('--model', type=str, help='The torch model to use')
+    parser.add_argument('--data', type=str, help='The folder of images to read')
+
+    return parser.parse_args()
+
+
+# Create the ANNOY indexes from a given directory
 def main():
-    device = torch.device('cuda')
-    model = DeepRank().cuda(device)
+    args = parse_args()
+    if args.cuda and not torch.cuda.is_available():
+        raise Exception('CUDA Device not found')
 
-    input_image = Image.open('test.jpg').convert('RGB')
-    input_image = input_image.resize((224, 224), Image.BICUBIC)
-    input_tensor = Variable(ToTensor()(input_image))
-    input_tensor = input_tensor.view(1, -1, input_image.size[1], input_image.size[0])
+    device = torch.device('cuda' if args.cuda else 'cpu')
+    model = torch.load(args.model)
+    model.eval()
 
-    model(input_tensor.cuda())
+    annoy = AnnoyIndex(4096)
+
+    for image in get_images(args.data):
+        input_image = Image.open(image).convert('RGB')
+        input_tensor = Variable(ToTensor()(input_image))
+        input_tensor = input_tensor.view(1, -1, input_image.size[1], input_image.size[0])
+
+        if args.cuda:
+            input_tensor = input_tensor.to(device)
+            model = model.to(device)
+
+        output = model(input_tensor)
+        if args.cuda:
+            output = output.cpu()
+
+        # Add the embedding to the index.
+        annoy.add_item(output)
+
+    annoy.build(20)
+    annoy.save('embeddings.ann')
 
 
 if __name__ == '__main__':
